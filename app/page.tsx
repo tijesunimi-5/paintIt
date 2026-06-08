2970127cf33b7fafdca99e7c48513ae0f1b33ff7
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2026-06-08 13:48:26 +0100

    removed future vision

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             <Features />
             <HowItWorks />
             <EarlyAccessForm />
-            <FutureVision />
+            {/* <FutureVision /> */}
             <Footer />
           </div>
 
